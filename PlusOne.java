1b295fc3b1faef7dabb00a829c65029e7d617660
mayankawalpane <mayankawalpane25@gmail.com>
2025-10-31 23:49:42 +0530

    Add PlusOne class to implement array increment functionality

---
+++ PlusOne.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class PlusOne {
+    public static void main(String[] args) {
+
+        int digits[] = {4,3,2,1};
+
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                printArray(digits);
+                return;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        printArray(newNumber);
+    }
+
+    static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
