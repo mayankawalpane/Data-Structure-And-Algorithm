0304f3e4b059ef04fd1f82dc30432ca9fac80649
mayankawalpane <mayankawalpane25@gmail.com>
2025-11-17 23:39:20 +0530

    dad

--- Arrays/SingleNumber.java
+++ Arrays/SingleNumber.java
@@ -1,29 +1,30 @@
-import java.util.*;
 
+import java.util.*;
 
 public class SingleNumber {
 
     public static void main(String[] args) {
-        
-     int nums[] ={2,2,1,5,5,4,4};
 
+        int nums[] = { 2, 2, 1, 5, 5, 4, 4 };
 
-     for(int i=0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < nums.length; j++) {
 
-        for(int j=1;j<nums.length;j++){
+                if (i != j && nums[i] == nums[j]) {
 
-            if(nums[i]==nums[j]){
-                return ;
-            }else{
+                    isUnique = false;
+                    break;
 
-                   System.out.println(nums[i]);
+                }
+
+            }
+            if (isUnique) {
+                System.out.println(nums[i]);
+                break;
             }
         }
 
-     }
-
-
     }
 
-    
 }
